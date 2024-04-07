73aaa58c4e2c231de0521f8be0d9ec52ee8ddef3
southorange0929 <southorange0929@foxmail.com>
2024-04-07 10:30:06 +0800

    refactor: rename to canIUse

--- crates/init/src/lib.rs
+++ crates/init/src/lib.rs
@@ -3,7 +3,8 @@ use std::ffi::CString;
 mod sys;
 
 /// check cap can be used, implement with canIUse
-pub fn can_i_use(cap: &str) -> bool {
+#[allow(non_snake_case)]
+pub fn canIUse(cap: &str) -> bool {
     let c_cap = CString::new(cap).expect("Create c_string failed");
     unsafe {
         sys::canIUse(c_cap.as_ptr().cast())
