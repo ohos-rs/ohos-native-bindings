ad4fa4b1375a704923822565276117417b8b53b4
richerfu <southorange0929@foxmail.com>
2025-03-03 16:55:36 +0800

    refactor(vsync): refactor vsync and and vsync example

--- crates/vsync/src/lib.rs
+++ crates/vsync/src/lib.rs
@@ -1,50 +1,107 @@
-use std::{cell::RefCell, ffi::CString, os::raw::c_void};
+use std::{ffi::CString, os::raw::c_void, ptr::NonNull};
 
 use ohos_vsync_sys::{
     OH_NativeVSync, OH_NativeVSync_Create, OH_NativeVSync_Destroy, OH_NativeVSync_GetPeriod,
     OH_NativeVSync_RequestFrame, OH_NativeVSync_RequestFrameWithMultiCallback,
 };
 
-pub struct Vsync(*const OH_NativeVSync);
+use std::marker::PhantomData;
 
-thread_local! {
-    static VSYNC_HANDLE: RefCell<Option<Box<dyn Fn(i64, *mut c_void) + 'static>>> = RefCell::new(None);
+pub struct Vsync<'a> {
+    raw: NonNull<OH_NativeVSync>,
+    _phantom: PhantomData<&'a ()>,
 }
 
-impl Vsync {
+unsafe impl<'a> Send for Vsync<'a> {}
+unsafe impl<'a> Sync for Vsync<'a> {}
+
+struct VsyncData {
+    callback: Box<dyn FnMut(i64)>,
+    raw: NonNull<OH_NativeVSync>,
+}
+
+impl<'a> Vsync<'a> {
     pub fn new<T: AsRef<str>>(name: T) -> Self {
         let name = CString::new(name.as_ref()).expect("CString::new failed");
         let vsync =
             unsafe { OH_NativeVSync_Create(name.as_ptr().cast(), name.to_bytes().len() as u32) };
-        Vsync(vsync)
+        Vsync {
+            raw: NonNull::new(vsync).expect("OH_NativeVSync_Create failed"),
+            _phantom: PhantomData,
+        }
     }
 
-    pub fn on_frame<F: Fn(i64, *mut c_void) + 'static>(&self, data: *mut c_void, callback: F) {
-        VSYNC_HANDLE.with_borrow_mut(|f| {
-            *f = Some(Box::new(callback));
+    /// This function is used to handle the vsync event.
+    ///
+    /// The callback function will be called when the vsync event occurs.
+    pub fn on_frame<F: FnMut(i64) + 'a>(&self, mut callback: F) {
+        let callback_with_data = unsafe {
+            std::mem::transmute::<Box<dyn FnMut(i64)>, Box<dyn FnMut(i64) + 'static>>(Box::new(
+                |time: i64| {
+                    callback(time);
+                },
+            ))
+        };
+
+        let data = Box::new(VsyncData {
+            callback: Box::new(callback_with_data),
+            raw: self.raw,
         });
+
         unsafe {
             OH_NativeVSync_RequestFrame(
-                self.0 as *mut OH_NativeVSync,
+                self.raw.as_ptr(),
                 Some(request_frame_callback),
-                data,
+                Box::into_raw(data) as _,
             );
         }
     }
 
-    pub fn on_frame_with_multi_callback<F: Fn(i64, *mut c_void) + 'static>(
-        &self,
-        data: *mut c_void,
-        callback: F,
-    ) {
-        VSYNC_HANDLE.with_borrow_mut(|f| {
-            *f = Some(Box::new(callback));
+    /// This function is used to handle the vsync event with multiple callbacks.
+    pub fn on_frame_with_multi_callback<F: FnMut(i64) + 'a>(&self, mut callback: F) {
+        let f = unsafe {
+            std::mem::transmute::<Box<dyn FnMut(i64)>, Box<dyn FnMut(i64) + 'static>>(Box::new(
+                move |t| {
+                    callback(t);
+                },
+            ))
+        };
+        let data = Box::new(VsyncData {
+            callback: Box::new(f),
+            raw: self.raw,
         });
         unsafe {
             OH_NativeVSync_RequestFrameWithMultiCallback(
-                self.0 as *mut OH_NativeVSync,
+                self.raw.as_ptr(),
                 Some(request_frame_callback),
-                data,
+                Box::into_raw(data) as _,
+            );
+        }
+    }
+
+    /// This function is used to handle the vsync event with every frame.
+    ///
+    /// The callback function will be called every frame, and the callback function can call
+    /// `OH_NativeVSync_RequestFrame` to request the next frame.
+    pub fn on_frame_with_self<F: FnMut(i64) + 'a>(&self, mut callback: F) {
+        let callback_with_data = unsafe {
+            std::mem::transmute::<Box<dyn FnMut(i64)>, Box<dyn FnMut(i64) + 'static>>(Box::new(
+                |time: i64| {
+                    callback(time);
+                },
+            ))
+        };
+
+        let data = Box::new(VsyncData {
+            callback: Box::new(callback_with_data),
+            raw: self.raw,
+        });
+
+        unsafe {
+            OH_NativeVSync_RequestFrame(
+                self.raw.as_ptr(),
+                Some(request_frame_callback_with_self),
+                Box::into_raw(data) as _,
             );
         }
     }
@@ -52,22 +109,35 @@ impl Vsync {
     pub fn period(&self) -> i64 {
         let mut period = 0;
         unsafe {
-            OH_NativeVSync_GetPeriod(self.0 as *mut OH_NativeVSync, &mut period);
+            OH_NativeVSync_GetPeriod(self.raw.as_ptr(), &mut period);
         }
         period
     }
 }
 
 extern "C" fn request_frame_callback(timestamp: i64, data: *mut c_void) {
-    VSYNC_HANDLE.with_borrow(|f| {
-        if let Some(f) = f.as_ref() {
-            f(timestamp, data);
-        }
-    })
+    let data = unsafe { &mut *(data as *mut VsyncData) };
+    let handle = &mut data.callback;
+    handle(timestamp);
 }
 
-impl Drop for Vsync {
+extern "C" fn request_frame_callback_with_self(timestamp: i64, data: *mut c_void) {
+    let raw_data = unsafe { &mut *(data as *mut VsyncData) };
+    let handle = &mut raw_data.callback;
+    // request next frame
+    unsafe {
+        OH_NativeVSync_RequestFrame(
+            raw_data.raw.as_ptr(),
+            Some(request_frame_callback_with_self),
+            data as _,
+        );
+    }
+    // handle current frame
+    handle(timestamp);
+}
+
+impl<'a> Drop for Vsync<'a> {
     fn drop(&mut self) {
-        unsafe { OH_NativeVSync_Destroy(self.0 as *mut OH_NativeVSync) }
+        unsafe { OH_NativeVSync_Destroy(self.raw.as_ptr()) }
     }
 }
---
+++ examples/vsync/src/lib.rs
@@ -0,0 +1,22 @@
+use std::sync::LazyLock;
+
+use napi_derive_ohos::napi;
+use ohos_hilog_binding::hilog_info;
+use ohos_vsync_binding::Vsync;
+
+static VSYNC: LazyLock<Vsync> = LazyLock::new(|| Vsync::new("vsync"));
+
+#[napi]
+pub fn handle_vsync() {
+    VSYNC.on_frame(|s| {
+        hilog_info!("vsync: {}", s);
+    });
+}
+
+#[napi]
+pub fn handle_vsync_with_self() {
+    let a = 1;
+    VSYNC.on_frame_with_self(move |s| {
+        hilog_info!("vsync: {} {}", s, a);
+    });
+}
