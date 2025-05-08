5e222ed34fb256fa7c7dae084b6fa716959da0cb
richerfu <southorange0929@foxmail.com>
2025-05-08 19:16:05 +0800

    fix: use NonNull to replace raw pointer

--- crates/ime/src/proxy/mod.rs
+++ crates/ime/src/proxy/mod.rs
@@ -30,7 +30,7 @@ fn char16_ptr_to_string(ptr: *const u16, length: usize) -> String {
 }
 
 pub unsafe extern "C" fn delete_backward(_text_editor: *mut InputMethod_TextEditorProxy, len: i32) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.delete_backward {
         f(len);
     }
@@ -41,15 +41,15 @@ pub unsafe extern "C" fn insert_text(
     text: *const char16_t,
     len: usize,
 ) {
-    let mut guard = IME_CALLBACKS.write().expect("Failed to acquire read lock");
-    if let Some(ref mut f) = guard.insert_text {
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    if let Some(f) = &guard.insert_text {
         let ret = char16_ptr_to_string(text, len);
         f(ret);
     }
 }
 
 pub unsafe extern "C" fn delete_forward(_text_editor: *mut InputMethod_TextEditorProxy, len: i32) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
 
     if let Some(f) = &guard.delete_forward {
         f(len);
@@ -57,7 +57,7 @@ pub unsafe extern "C" fn delete_forward(_text_editor: *mut InputMethod_TextEdito
 }
 
 pub unsafe extern "C" fn finish_text_preview(_text_editor: *mut InputMethod_TextEditorProxy) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.finish_text_preview {
         f();
     }
@@ -69,7 +69,7 @@ pub unsafe extern "C" fn get_left_text_of_cursor(
     text: *mut char16_t,
     len: *mut usize,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.get_left_text_of_cursor {
         let s = f(number);
         let utf16: Vec<u16> = s.encode_utf16().collect();
@@ -87,7 +87,7 @@ pub unsafe extern "C" fn get_right_text_of_cursor(
     text: *mut char16_t,
     len: *mut usize,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.get_right_text_of_cursor {
         let s = f(number);
         let utf16: Vec<u16> = s.encode_utf16().collect();
@@ -103,7 +103,7 @@ pub unsafe extern "C" fn get_text_config(
     _text_editor: *mut InputMethod_TextEditorProxy,
     config: *mut InputMethod_TextConfig,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.get_text_config {
         f(TextConfig { raw: config });
     }
@@ -112,7 +112,7 @@ pub unsafe extern "C" fn get_text_config(
 pub unsafe extern "C" fn get_text_index_at_cursor(
     _text_editor: *mut InputMethod_TextEditorProxy,
 ) -> i32 {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     match &guard.get_text_index_at_cursor {
         Some(f) => {
             let ret = f();
@@ -126,7 +126,7 @@ pub unsafe extern "C" fn handle_extend_action(
     _text_editor: *mut InputMethod_TextEditorProxy,
     action: InputMethod_ExtendAction,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.handle_extend_action {
         f(Action::from(action));
     }
@@ -137,7 +137,7 @@ pub unsafe extern "C" fn handle_set_selection(
     start: i32,
     end: i32,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.handle_set_selection {
         f(Selection { start, end });
     }
@@ -147,7 +147,7 @@ pub unsafe extern "C" fn move_cursor(
     _text_editor: *mut InputMethod_TextEditorProxy,
     direction: InputMethod_Direction,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.move_cursor {
         f(Direction::from(direction));
     }
@@ -158,7 +158,7 @@ pub unsafe extern "C" fn receive_private_command(
     command: *mut *mut InputMethod_PrivateCommand,
     len: usize,
 ) -> i32 {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.receive_private_command {
         unsafe {
             let slice = std::slice::from_raw_parts_mut(command, len);
@@ -179,7 +179,7 @@ pub unsafe extern "C" fn send_enter_key(
     _text_editor: *mut InputMethod_TextEditorProxy,
     enter_key_type: InputMethod_EnterKeyType,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.send_enter_key {
         f(EnterKey::from(enter_key_type));
     }
@@ -189,7 +189,7 @@ pub unsafe extern "C" fn send_keyboard_status(
     _text_editor: *mut InputMethod_TextEditorProxy,
     keyboard_status: InputMethod_KeyboardStatus,
 ) {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.send_keyboard_status {
         f(KeyboardStatus::from(keyboard_status));
     }
@@ -202,7 +202,7 @@ pub unsafe extern "C" fn set_preview_text(
     start: i32,
     end: i32,
 ) -> i32 {
-    let guard = IME_CALLBACKS.read().expect("Failed to acquire read lock");
+    let guard = OHOS_RS_IME_CALLBACKS.read().expect("Failed to acquire read lock");
     if let Some(f) = &guard.set_preview_text {
         let ret = char16_ptr_to_string(text, length);
         f(ret, start, end);
