62c12d2d2e71de746d658130e4010e55c71cce09
richerfu <32590310+richerfu@users.noreply.github.com>
2025-05-12 15:38:41 +0800

    feat(ime): add more method (#51)
    * feat(ime): add more method
    
    * feat: add more method
    
    * chore: format code

--- crates/ime/src/ime.rs
+++ crates/ime/src/ime.rs
@@ -8,7 +8,7 @@ use std::{
     rc::Rc,
 };
 
-use crate::{proxy::OHOS_RS_IME_CALLBACKS, AttachOptions, KeyboardStatus, TextEditor};
+use crate::{proxy::OHOS_RS_IME_CALLBACKS, AttachOptions, EnterKey, KeyboardStatus, TextEditor};
 
 unsafe impl Send for IME {}
 unsafe impl Sync for IME {}
@@ -129,6 +129,65 @@ impl IME {
             #[cfg(debug_assertions)]
             assert!(ret == 0, "OH_InputMethodController_Attach failed");
         }
+    }
+
+    pub fn on_backspace<'a, T>(&self, callback: T)
+    where
+        T: Fn(i32) -> () + 'a,
+    {
+        let mut guard = OHOS_RS_IME_CALLBACKS
+            .write()
+            .expect("OHOS_RS_IME_CALLBACKS write failed");
+        let cb = unsafe {
+            std::mem::transmute::<Box<dyn Fn(i32) + 'a>, Box<dyn Fn(i32) + 'static>>(Box::new(
+                callback,
+            ))
+        };
+        (*guard).delete_backward = Some(cb);
+    }
+
+    pub fn on_enter<'a, T>(&self, callback: T)
+    where
+        T: Fn(EnterKey) -> () + 'a,
+    {
+        let mut guard = OHOS_RS_IME_CALLBACKS
+            .write()
+            .expect("OHOS_RS_IME_CALLBACKS write failed");
+        let cb = unsafe {
+            std::mem::transmute::<Box<dyn Fn(EnterKey) + 'a>, Box<dyn Fn(EnterKey) + 'static>>(
+                Box::new(callback),
+            )
+        };
+        (*guard).send_enter_key = Some(cb);
+    }
+
+    pub fn on_preview<'a, T>(&self, callback: T)
+    where
+        T: Fn(String, i32, i32) -> () + 'a,
+    {
+        let mut guard = OHOS_RS_IME_CALLBACKS
+            .write()
+            .expect("OHOS_RS_IME_CALLBACKS write failed");
+        let cb = unsafe {
+            std::mem::transmute::<
+                Box<dyn Fn(String, i32, i32) + 'a>,
+                Box<dyn Fn(String, i32, i32) + 'static>,
+            >(Box::new(callback))
+        };
+        (*guard).set_preview_text = Some(cb);
+    }
+
+    pub fn on_finish_preview<'a, T>(&self, callback: T)
+    where
+        T: Fn() -> () + 'a,
+    {
+        let mut guard = OHOS_RS_IME_CALLBACKS
+            .write()
+            .expect("OHOS_RS_IME_CALLBACKS write failed");
+        let cb = unsafe {
+            std::mem::transmute::<Box<dyn Fn() + 'a>, Box<dyn Fn() + 'static>>(Box::new(callback))
+        };
+        (*guard).finish_text_preview = Some(cb);
     }
 
     pub fn hide_keyboard(&self) {
