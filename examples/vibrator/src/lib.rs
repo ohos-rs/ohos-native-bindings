3828185ad6a19f0b4fd5cc6f23145defc433c7fc
southorange0929 <southorange0929@foxmail.com>
2025-10-07 21:33:00 +0800

    fix: example

--- examples/vibrator/src/lib.rs
+++ examples/vibrator/src/lib.rs
@@ -5,33 +5,38 @@ use napi_ohos::{Error, Result};
 use ohos_vibrator_binding::{Attribute, FileDescription, VibratorUsage};
 
 #[napi]
-pub fn cancel() {
-    ohos_vibrator_binding::cancel().unwrap();
+pub fn cancel() -> Result<()> {
+    ohos_vibrator_binding::cancel().map_err(|e| Error::from_reason(e.to_string()))?;
+    Ok(())
 }
 
 #[napi]
-pub fn start() {
+pub fn start() -> Result<()> {
     let attribute = Attribute {
         usage: VibratorUsage::Alarm,
         ..Default::default()
     };
-    ohos_vibrator_binding::start(0, attribute).unwrap();
+    ohos_vibrator_binding::start(5000, attribute).map_err(|e| Error::from_reason(e.to_string()))?;
+    Ok(())
 }
 
 #[napi]
-pub fn custom_start() -> Result<()> {
-    let file_info = fs::File::open("/data/test/vibrator/coin_drop.json")
-        .map_err(|e| Error::from_reason(e.to_string()))?;
+pub fn custom_start(file_path: String) -> Result<()> {
+    // /data/storage/el2/base/haps/entry/files/coin_drop.json
+    let file_info = fs::File::open(file_path).map_err(|e| Error::from_reason(e.to_string()))?;
     let file_description = FileDescription {
         fd: file_info.as_raw_fd(),
         offset: 0,
-        length: file_info.metadata().map_err(|e| Error::from_reason(e.to_string()))?.len(),
+        length: file_info
+            .metadata()
+            .map_err(|e| Error::from_reason(e.to_string()))?
+            .len(),
     };
     let attribute = Attribute {
-        usage: VibratorUsage::Alarm,
+        usage: VibratorUsage::Ring,
         ..Default::default()
     };
-    ohos_vibrator_binding::custom_start(file_description, attribute).unwrap();
-
+    ohos_vibrator_binding::custom_start(file_description, attribute)
+        .map_err(|e| Error::from_reason(e.to_string()))?;
     Ok(())
 }
