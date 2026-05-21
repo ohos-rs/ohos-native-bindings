74be9d335a796c3b4c20f8a0fcee2075c62ca23e
ljlvink <qpalzmlong@foxmail.com>
2026-05-21 17:09:16 +0800

    chore: cargo fmt

--- tools/generate/build/config/hms_opengtx.rs
+++ tools/generate/build/config/hms_opengtx.rs
@@ -5,7 +5,7 @@ use super::SysConfig;
 pub const OPENGTX: Lazy<SysConfig> = Lazy::new(|| SysConfig {
     name: "hms-opengtx-sys",
     headers: vec!["graphics_game_sdk/opengtx_base.h"],
-    white_list: vec!["HMS_OpenGTX.*","OpenGTX_*"],
+    white_list: vec!["HMS_OpenGTX.*", "OpenGTX_*"],
     block_list: vec![],
     dynamic_library: vec!["opengtx"],
     extra: "",
